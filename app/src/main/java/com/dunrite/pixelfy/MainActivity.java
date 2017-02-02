c96d84445060e337c3663a81a5771c3f46969dcd
JaxonWright <jaxonhwright@gmail.com>
2017-02-02 15:07:46 -0500

    Start mock navbar to test positioning

--- app/src/main/java/com/dunrite/pixelfy/MainActivity.java
+++ app/src/main/java/com/dunrite/pixelfy/MainActivity.java
@@ -2,12 +2,95 @@ package com.dunrite.pixelfy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.serviceToggle) Switch serviceToggle;
+    @BindView(R.id.scaleBar) SeekBar scaleBar;
+    @BindView(R.id.spacingBar) SeekBar spacingBar;
+    @BindView(R.id.homeButton) ImageView homeButton;
+    @BindView(R.id.backButton) ImageView backButton;
+    @BindView(R.id.recentsButton) ImageView recentsButton;
+    private final int NAVBAR_HEIGHT_IN_DP = 48;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initialize();
+    }
+
+    private void initialize() {
+        //setSpacing(spacingBar.getProgress());
+        //setScale();
+
+        spacingBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                System.out.println("Spacing set to " + progress);
+                if (fromUser)
+                    setSpacing(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        scaleBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setScale(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void setSpacing(int spacing) {
+        ViewGroup.MarginLayoutParams marginParams = (ViewGroup.MarginLayoutParams) homeButton.getLayoutParams();
+        marginParams.setMargins(spacing, 0, spacing, 0);
+        homeButton.requestLayout();
+    }
+
+    private void setScale(double scale) {
+        double realScale = convertDpToPx(NAVBAR_HEIGHT_IN_DP * (scale/100));
+        homeButton.getLayoutParams().height = (int) realScale;
+        backButton.getLayoutParams().height = (int) realScale;
+        recentsButton.getLayoutParams().height = (int) realScale;
+
+        homeButton.requestLayout();
+        backButton.requestLayout();
+        recentsButton.requestLayout();
+    }
+
+    private double convertDpToPx(double dp) {
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 }
+
