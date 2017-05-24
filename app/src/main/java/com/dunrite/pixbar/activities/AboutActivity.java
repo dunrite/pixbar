b61944c0b352471d03166c90046b222ac8c153a7
JaxonWright <jaxonhwright@gmail.com>
2017-05-24 18:41:58 -0400

    Add email and github links in about activity. Makes easier for users to report issues

--- app/src/main/java/com/dunrite/pixbar/activities/AboutActivity.java
+++ app/src/main/java/com/dunrite/pixbar/activities/AboutActivity.java
@@ -1,26 +1,37 @@
 package com.dunrite.pixbar.activities;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.dunrite.pixbar.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * About activity
  */
 public class AboutActivity extends AppCompatActivity {
     //@BindView(R.id.debugtext) TextView debugText;
+    @BindView(R.id.emailButton) Button emailButton;
+    @BindView(R.id.githubButton) Button githubButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-      //  ButterKnife.bind(this);
+        ButterKnife.bind(this);
         getSupportActionBar().setTitle("About");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         String versionName = "";
@@ -41,6 +52,24 @@ public class AboutActivity extends AppCompatActivity {
 //                          "\nBRAND: " + Build.BRAND +
 //                          "\nMODEL: " + Build.MODEL +
 //                          "\nPRODUCT: " + Build.PRODUCT);
+    }
+
+    @OnClick(R.id.emailButton)
+    public void startEmail() {
+        ShareCompat.IntentBuilder.from(this)
+                .setType("message/rfc822")
+                .addEmailTo("support@dunriteapps.com")
+                .setSubject("Pixbar")
+                .setText("Describe your issue here...")
+                .setChooserTitle("Send email...")
+                .startChooser();
+    }
+
+    @OnClick(R.id.githubButton)
+    public void openGithub() {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://github.com/dunrite/pixbar/issues"));
+        startActivity(i);
     }
 
     @Override
